c1fc5f7bacf28bbb1434502d507afa22a96734e2
Kolby <kolbykalafut@gmail.com>
2018-02-26 00:42:49 -0500

    Assignment 27 completed

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,4 +1,7 @@
 module TopicsHelper
+  def user_is_authorized_for_topic_edit?
+    current_user && current_user.admin? || current_user && current_user.moderator?
+  end
   def user_is_authorized_for_topics?
     current_user && current_user.admin?
   end
